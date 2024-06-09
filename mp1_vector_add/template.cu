a38335baa1be7000e4d9ced5844eb01f890ea265
wylin2 <wylin2@node-1.weiyu2.gaea-pg0.wisc.cloudlab.us>
2024-06-09 18:24:52 +0000

    Initial vector addition implementation

--- mp1_vector_add/template.cu
+++ mp1_vector_add/template.cu
@@ -1,45 +1,73 @@
+#include <cuda_device_runtime_api.h>
 #include <cuda_runtime.h>
+#include <cuda_runtime_api.h>
+#include <driver_types.h>
 #include <iostream>
 #include <chrono>
+#include <stdio.h>
 
-__global__ void VecAdd(float* A, float* B, float* C)
+__global__ void VecAdd(float* A, float* B, float* C, int N)
 {
     int i = threadIdx.x;
-    C[i] = A[i] + B[i];
+    if (i < N){
+     C[i] = A[i] + B[i];
+    }
 }
 
 int main(int argc, char** argv){
+
+  cudaDeviceProp deviceProp;
+  printf("Device Compute Capability: %d.%d\n", deviceProp.major, deviceProp.minor);
   // Data size
   int N = 256;
 
   // Init 3 host memories
-  float *host_A = (float)malloc(N * sizeof(float));
-  float *host_B = (float)malloc(N * sizeof(float));
-  float *host_C = (float)malloc(N * sizeof(float));
+  float *host_A = (float*)malloc(N * sizeof(float));
+  float *host_B = (float*)malloc(N * sizeof(float));
+  float *host_C = (float*)malloc(N * sizeof(float));
 
   // Init 3 device memories
-  float *device_A = (float)malloc(N * sizeof(float));
-  float *device_B = (float)malloc(N * sizeof(float));
-  float *device_C = (float)malloc(N * sizeof(float));
-  
-  // Init data
+  float *device_A, *device_B, *device_C;
+  cudaMalloc((void**)&device_A, N * sizeof(float));
+  cudaMalloc((void**)&device_B, N * sizeof(float));
+  cudaMalloc((void**)&device_C, N * sizeof(float));
+
+  // Init host data
+  for (int i = 0; i < N; i ++) {
+    host_A[i] = float(i);
+    host_B[i] = float(i);
+  }
+  // std::cout << host_B[5];
   
   // Copy data from host -> device
+  cudaMemcpy(device_A, host_A, N * sizeof(float), cudaMemcpyHostToDevice); 
+  cudaMemcpy(device_B, host_B, N * sizeof(float), cudaMemcpyHostToDevice); 
 
   // Launch kernel
   auto start = std::chrono::high_resolution_clock::now();
-   
-  auto stop = std::chrono::high_resolution_clock::now();
+  VecAdd<<<1, N>>>(device_A, device_B, device_C, N); 
+
+  // Copy result from device -> host
+  cudaMemcpy(host_C, device_C, N * sizeof(float), cudaMemcpyDeviceToHost); 
+  auto stop = std::chrono::high_resolution_clock::now();  
   
-  // Copy data from device -> host_A
+  // Print result
+  std::cout << "Result:";
+  for (int i = 0; i < N; i ++) {
+    std::cout << host_C[i] << " ";  
+  }
+  std::cout << std::endl;
+  
+  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+  std::cout << "Time spent:" << duration.count()  << "ms" << std::endl;
 
   // Free memory
   cudaFree(device_A);
   cudaFree(device_B);
-  cudaFree(decice_C);
+  cudaFree(device_C);
   free(host_A);
-  free(host_b);
-  free(host_c);
+  free(host_B);
+  free(host_C);
 
   return 0;
 }
