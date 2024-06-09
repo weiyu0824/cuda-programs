dd111af83d918690d57ec561d4ab18966ec0a31f
wylin2 <wylin2@node-1.weiyu2.gaea-pg0.wisc.cloudlab.us>
2024-06-09 19:40:39 +0000

    Complete vector addition

--- mp1_vector_add/template.cu
+++ mp1_vector_add/template.cu
@@ -8,18 +8,16 @@
 
 __global__ void VecAdd(float* A, float* B, float* C, int N)
 {
-    int i = threadIdx.x;
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
     if (i < N){
-     C[i] = A[i] + B[i];
+      C[i] = A[i] + B[i];
     }
 }
 
 int main(int argc, char** argv){
 
-  cudaDeviceProp deviceProp;
-  printf("Device Compute Capability: %d.%d\n", deviceProp.major, deviceProp.minor);
   // Data size
-  int N = 256;
+  int N = 10240;
 
   // Init 3 host memories
   float *host_A = (float*)malloc(N * sizeof(float));
@@ -37,30 +35,37 @@ int main(int argc, char** argv){
     host_A[i] = float(i);
     host_B[i] = float(i);
   }
-  // std::cout << host_B[5];
   
   // Copy data from host -> device
   cudaMemcpy(device_A, host_A, N * sizeof(float), cudaMemcpyHostToDevice); 
   cudaMemcpy(device_B, host_B, N * sizeof(float), cudaMemcpyHostToDevice); 
 
+  // 
+  dim3 threadsPerBlock(32);
+  dim3 numBlocks(N/threadsPerBlock.x);
   // Launch kernel
   auto start = std::chrono::high_resolution_clock::now();
-  VecAdd<<<1, N>>>(device_A, device_B, device_C, N); 
+  VecAdd<<<numBlocks, threadsPerBlock>>>(device_A, device_B, device_C, N); 
 
   // Copy result from device -> host
   cudaMemcpy(host_C, device_C, N * sizeof(float), cudaMemcpyDeviceToHost); 
   auto stop = std::chrono::high_resolution_clock::now();  
   
-  // Print result
-  std::cout << "Result:";
-  for (int i = 0; i < N; i ++) {
-    std::cout << host_C[i] << " ";  
+  cudaError_t error = cudaGetLastError();
+  if (error != cudaSuccess) {
+      printf("CUDA error: %s\n", cudaGetErrorString(error));
+      // Handle error
+  }else{
+    // Print result
+    std::cout << "Result:";
+    for (int i = 0; i < N; i ++) {
+      std::cout << host_C[i] << " ";  
+    }
+    std::cout << std::endl;
+    
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    std::cout << "Time spent:" << duration.count()  << "ms" << std::endl;
   }
-  std::cout << std::endl;
-  
-  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-  std::cout << "Time spent:" << duration.count()  << "ms" << std::endl;
-
   // Free memory
   cudaFree(device_A);
   cudaFree(device_B);
