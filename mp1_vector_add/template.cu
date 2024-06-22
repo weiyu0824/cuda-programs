992f3602bc8928a83333510eb19452f1ad111940
wylin2 <wylin2@node-1.weiyu2.gaea-pg0.wisc.cloudlab.us>
2024-06-22 23:00:36 +0000

    Fix error in mp1 template.cu

--- mp1_vector_add/template.cu
+++ mp1_vector_add/template.cu
@@ -5,24 +5,33 @@
 #include <iostream>
 #include <chrono>
 #include <stdio.h>
+#include "../lib/vector_utils.hpp"
 
-__global__ void VecAdd(float* A, float* B, float* C, int N)
+__global__ void VecAdd(float* A, float* B, float* C, size_t N)
 {
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    size_t i = blockIdx.x * blockDim.x + threadIdx.x;
     if (i < N){
       C[i] = A[i] + B[i];
     }
 }
 
 int main(int argc, char** argv){
+  
 
-  // Data size
-  int N = 10240;
+  // std::cout << argc << std::endl;
+  if (argc != 4)
+    throw std::runtime_error("Expecting 3 arguments, input_a, input_b, and output");
+  
 
-  // Init 3 host memories
-  float *host_A = (float*)malloc(N * sizeof(float));
-  float *host_B = (float*)malloc(N * sizeof(float));
+  // Init host memories & data size 
+  size_t N;
+  float *host_A = vector_utils::read_vector<float>(argv[1], N);
+  float *host_B = vector_utils::read_vector<float>(argv[2], N);  
+  float *host_ans = vector_utils::read_vector<float>(argv[3], N);
   float *host_C = (float*)malloc(N * sizeof(float));
+
+  // vector_utils::print_vector<float>(host_A, N);
+  // vector_utils::print_vector<float>(host_B, N);
 
   // Init 3 device memories
   float *device_A, *device_B, *device_C;
@@ -30,19 +39,13 @@ int main(int argc, char** argv){
   cudaMalloc((void**)&device_B, N * sizeof(float));
   cudaMalloc((void**)&device_C, N * sizeof(float));
 
-  // Init host data
-  for (int i = 0; i < N; i ++) {
-    host_A[i] = float(i);
-    host_B[i] = float(i);
-  }
-  
-  // Copy data from host -> device
-  cudaMemcpy(device_A, host_A, N * sizeof(float), cudaMemcpyHostToDevice); 
+  cudaMemcpy(device_A, host_A, N * sizeof(float), cudaMemcpyHostToDevice);
   cudaMemcpy(device_B, host_B, N * sizeof(float), cudaMemcpyHostToDevice); 
 
   // 
   dim3 threadsPerBlock(32);
-  dim3 numBlocks(N/threadsPerBlock.x);
+  dim3 numBlocks((int) ceil(N/(float)threadsPerBlock.x));
+
   // Launch kernel
   auto start = std::chrono::high_resolution_clock::now();
   VecAdd<<<numBlocks, threadsPerBlock>>>(device_A, device_B, device_C, N); 
@@ -53,19 +56,22 @@ int main(int argc, char** argv){
   
   cudaError_t error = cudaGetLastError();
   if (error != cudaSuccess) {
-      printf("CUDA error: %s\n", cudaGetErrorString(error));
+    std::cout << "CUDA error: " << cudaGetErrorString(error) << std::endl;
       // Handle error
   }else{
-    // Print result
-    std::cout << "Result:";
-    for (int i = 0; i < N; i ++) {
-      std::cout << host_C[i] << " ";  
-    }
-    std::cout << std::endl;
-    
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-    std::cout << "Time spent:" << duration.count()  << "ms" << std::endl;
+    
+    // Log
+    if (vector_utils::compare_vector<float>(host_ans, host_C, N))
+      std::cout << "[o] ";
+    else{
+      vector_utils::print_vector(host_C, N);
+      vector_utils::print_vector(host_ans, N);
+      std::cout << "[x] ";
+    }
+    std::cout << "Data size:" << N << ", Time spent:" << duration.count()  << "ms" << std::endl;
   }
+
   // Free memory
   cudaFree(device_A);
   cudaFree(device_B);
